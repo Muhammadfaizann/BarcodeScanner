e3a494acf71614a796f68566eaf825ebf4689957
Muhammad Faizan <mfaizan@lcisystems.com>
2022-12-08 07:06:37 -0800

    initial commit

---
+++ BarcodeScanner/obj/Debug/net6.0-android/android/src/crc644ad926abd86a58d7/DataWedgeReceiver.java
@@ -0,0 +1,47 @@
+package crc644ad926abd86a58d7;
+
+
+public class DataWedgeReceiver
+	extends android.content.BroadcastReceiver
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onReceive:(Landroid/content/Context;Landroid/content/Intent;)V:GetOnReceive_Landroid_content_Context_Landroid_content_Intent_Handler\n" +
+			"";
+		mono.android.Runtime.register ("BarcodeScanner.Platforms.Android.DataWedgeReceiver, BarcodeScanner", DataWedgeReceiver.class, __md_methods);
+	}
+
+
+	public DataWedgeReceiver ()
+	{
+		super ();
+		if (getClass () == DataWedgeReceiver.class)
+			mono.android.TypeManager.Activate ("BarcodeScanner.Platforms.Android.DataWedgeReceiver, BarcodeScanner", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onReceive (android.content.Context p0, android.content.Intent p1)
+	{
+		n_onReceive (p0, p1);
+	}
+
+	private native void n_onReceive (android.content.Context p0, android.content.Intent p1);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
